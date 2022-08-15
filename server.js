df4ae17f8fe44191cae1908f12f13a4505ef9cca
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2022-08-16 05:02:14 +0600

    added

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', (req, res) => {
 app.use((err, req, res, next) => {
 	if(err){
 		console.log(err);
-		res.status(500).send("Something went wrong");
+		res.status(500).send(err);
 	}
 })
 
